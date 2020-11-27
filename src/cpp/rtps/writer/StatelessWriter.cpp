893935986a21c6a940d668e85cd76b2555d84ab6
Iker Luengo <ikerluengo@eprosima.com>
2020-11-27 20:09:50 +0100

    Refs 9671. Check datasharing compatibility on AUTO modes
    Signed-off-by: Iker Luengo <ikerluengo@eprosima.com>

--- src/cpp/rtps/writer/StatelessWriter.cpp
+++ src/cpp/rtps/writer/StatelessWriter.cpp
@@ -743,8 +743,7 @@ bool StatelessWriter::matched_reader_add(
         }
     }
 
-    if (is_datasharing_compatible_ &&
-            data.m_qos.data_sharing.kind() != fastdds::dds::DISABLED)
+    if (is_datasharing_compatible_with(data))
     {
 
         if (datasharing_notifier_->add_reader(data.guid()))
